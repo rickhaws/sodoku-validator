62e90ca092e9616f8a8ad4f59a0cfd926fef4c0d
Rick Haws <61323632+rickhaws@users.noreply.github.com>
2023-02-27 11:36:02 -0700

    Removed log statements

--- sudoku.js
+++ sudoku.js
@@ -11,19 +11,16 @@ var Sudoku = function(data)
   }
   
   function isValidSet(set) {
-//     /* */console.log('set', print(set));
     if (!set.every(x =>
       typeof x === "number" &&
       x >= 1 && 
       x <= set.length
     )) { 
-//       /* */ set.forEach(x => console.log(typeof x === "number", x >= 1, x <= set.length))
       return false; 
     }
     
     const counts = Array(set.length).fill(0);
     set.forEach(value => counts[value-1]++);
-//     /* */console.log('counts', print(counts))
     return counts.every(c => c===1);
   }
 
@@ -32,13 +29,9 @@ var Sudoku = function(data)
   // -------------------------
   return {
     isValid: function() {
-//       /* */console.log('data', print(data));
       const n = data[0].length;
-//       /* */ console.log('n', n)
       let valid = (data.length === n) && (data.every(row => row.length === n));
-//       /* */console.log('structure valid', valid);
       if(!valid) {
-//         /* */ console.log('invalid lengths')//, data.map(row => row.length));
       }
       
       if(valid) {
@@ -49,20 +42,17 @@ var Sudoku = function(data)
         const indices = [...data.keys()];
         const columns = indices.map(i => indices.map(j => data[j][i]));
         valid = columns.every(col => isValidSet(col));
-//         /* */console.log("columns", print(columns))
        }
       
       if(valid) {
         const nb = Math.sqrt(n);
         subIndices = [...Array(nb).keys()];
-//         /* */ console.log('subIndices', print(subIndices))
         const innerBlocks = 
           subIndices.flatMap(ib => 
             subIndices.map(jb => 
               subIndices.flatMap(i => 
                 subIndices.map(j => data[ib*nb + i][jb*nb + j]))));
                          
-//          /* */console.log('inner blocks', print(innerBlocks))
         valid = innerBlocks.every(block => isValidSet(block));
       } 
       
